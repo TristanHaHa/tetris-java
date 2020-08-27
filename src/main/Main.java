5fd205df0a38d3220d5da08d619b7506edb2aba0
Tristan-Ha <tristanha02@gmail.com>
2020-08-27 11:46:56 -0700

    renamed folder from 'code' to 'src'

---
+++ src/main/Main.java
@@ -0,0 +1,13 @@
+/**
+ * main method to run the Tetris game
+ *
+ * @author Tristan Ha
+ * @version 7/8/20
+ */
+public class Main
+{
+    public static void main(String[] args) {
+        MyFrame mf= new MyFrame();
+        mf.display();
+    }
+}
